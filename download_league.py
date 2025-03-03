d4307159f5deb7ddd820ec60b36416d10da49122
snorklecraft64 <70296357+snorklecraft64@users.noreply.github.com>
2025-03-03 23:25:16 +0000

    update download_league.py

--- download_league.py
+++ download_league.py
@@ -14,7 +14,7 @@ def execute_script_on_url(
     options = webdriver.ChromeOptions()
     # Stop browser windows to pop up.
     options.add_argument('--headless')
-    prefs = {"download.default_directory" : "/home/runner/work/download_league/download_league", 
+    prefs = {"download.default_directory" : os.path.dirname(os.path.realpath(__file__)), 
              'download.prompt_for_download': False,
              'download.directory_upgrade': True,
              'safebrowsing.enabled': True}
@@ -26,13 +26,13 @@ def execute_script_on_url(
         driver.get(page_url)
         time.sleep(load_wait_time)
         # Execute the JS script and wait for it to finish.
-        driver.execute_script(js_script)
+        result = driver.execute_script(js_script)
         time.sleep(script_wait_time)
+        return result
     finally:
         driver.quit()
 
 if __name__ == '__main__':
-    print(os.path.dirname(os.path.realpath(__file__)))
     # download standings from most recent week
     url = "https://www.leaguesecretary.com/bowling-centers/orleans-bowling-center/bowling-leagues/fat-purse-tuesday-ii-spring/league/standings/136652"
-    execute_script_on_url(url, "$(\"#gridStanding\").data(\'kendoGrid\').saveAsExcel()", 10, 10)
+    print(execute_script_on_url(url, "$(\"#gridStanding\").data(\'kendoGrid\').saveAsExcel()", 3, 1))
