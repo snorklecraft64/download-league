3cc478c3aace3ab1d8744b36089688da8d0ba835
snorklecraft64 <70296357+snorklecraft64@users.noreply.github.com>
2025-03-04 00:05:26 +0000

    update download_league.py

--- download_league.py
+++ download_league.py
@@ -39,4 +39,5 @@ if __name__ == '__main__':
     ###print(execute_script_on_url(url, "$(\"#gridStanding\").data(\'kendoGrid\').saveAsExcel()", 3, 1))
     print(execute_script_on_url(url, "$(\"#gridBowlerList\").data(\'kendoGrid\').saveAsExcel()", 3, 1))
     print(os.path.dirname(os.path.realpath(__file__)))
+    print(os.getcwd())
     print(os.listdir(os.path.dirname(os.path.realpath(__file__)) + "/ExcelSheets"))
